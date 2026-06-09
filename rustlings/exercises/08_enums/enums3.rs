d64997e4dffc65c4503718905abbc76c7606ecee
Bernardo Andrade <bernardocandrade6@gmail.com>
2026-06-09 09:01:08 -0300

    resolve rustlings de enums

--- rustlings/exercises/08_enums/enums3.rs
+++ rustlings/exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize{width: new_width, height: new_height} => self.resize(new_width, new_height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(message_to_echo) => self.echo(message_to_echo),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
